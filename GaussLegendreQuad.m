3917fe87bcbc7f7e3fa12bc70e190ec002290746
PourRevenir <yyk39693232@outlook.com>
2024-11-08 03:19:49 +0800

    Init

---
+++ GaussLegendreQuad.m
@@ -0,0 +1,5 @@
+function Integral = GaussLegendreQuad(point1,point2,func,n)
+%UNTITLED4 此处显示有关此函数的摘要
+%   此处显示详细说明
+
+end
---
+++ MultiGaussLegendreQuad.m
@@ -0,0 +1,5 @@
+function multiIntergral = MultiGaussLegendreQuad(point1,point2,func,n)
+%UNTITLED6 此处显示有关此函数的摘要
+%   此处显示详细说明
+
+end
