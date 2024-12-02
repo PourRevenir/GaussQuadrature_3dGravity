afae53ad90bb3ac3f58fdd517b6806a98cc38cd8
PourRevenir <yyk39693232@outlook.com>
2024-12-03 04:24:00 +0800

    Gauss-Legendre & Gauss-Chebyshev quadratures

---
+++ GaussChebyshevCoef.m
@@ -0,0 +1,16 @@
+function [w,x] = GaussChebyshevCoef(n)
+%GaussChebyshevCoef Calculate Gauss-Legendre quadrature coefficients and
+%Gaussian points.
+%
+%   [w,x] = GaussChebyshevCoef(n)
+%
+%   Input
+%       n - number of Gaussian points
+%   Output
+%       w - Gauss-Chebyshev quadrature coefficients
+%       x - Gaussian points
+%
+
+    w = pi/n*ones(1,n);
+    x = cos(((1:n)-0.5)*pi/n);
+end
---
+++ GaussLegendreQuad3.m
@@ -0,0 +1,34 @@
+function I = GaussLegendreQuad3(func,a1,a2,b1,b2,c1,c2,n1,n2,n3)
+%GaussLegendreQuad3 Computing triple integrals of the  function using
+%Gauss-Legendre quadrature.
+%   
+%   I = GaussLegendreQuad3(func,a1,a2,b1,b2,c1,c2,n1,n2,n3)
+%   
+%   Input
+%       func - function to be computed
+%       a1,a2,b1,b2,c1,c2 - model parameter
+%       n1,n2,n3 - number of Gaussian points
+%       
+%   Output
+%       I - integrals of the function
+%
+
+[wi,x] = GaussLegendreCoef(n1);
+[wj,y] = GaussLegendreCoef(n2);
+[wk,z] = GaussLegendreCoef(n3);
+
+wi = 0.5*(a2-a1)*wi;
+wj = 0.5*(b2-b1)*wj;
+wk = 0.5*(c2-c1)*wk;
+
+x = 0.5*((a2-a1)*x+a1+a2);
+y = 0.5*((b2-b1)*y+b1+b2);
+z = 0.5*((c2-c1)*z+c1+c2);
+
+[wi,wj,wk] = meshgrid(wi,wj,wk);
+[x,y,z] = meshgrid(x,y,z);
+
+I = wi.*wj.*wk.*func(x,y,z);
+I = sum(I(:));
+
+end
