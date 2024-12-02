f59db1486d60d45a51ca701687e8e3861c090ad3
PourRevenir <yyk39693232@outlook.com>
2024-12-03 06:00:34 +0800

    Dec 3, 2024 06:00 committed by yang

--- GaussLegendreCof.m
+++
@@ -1,5 +0,0 @@
-function [w,x] = GaussLegendreCof(n)
-%UNTITLED3 此处显示有关此函数的摘要
-%   此处显示详细说明
-
-end
--- GaussLegendreQuad.m
+++
@@ -1,5 +0,0 @@
-function Integral = GaussLegendreQuad(point1,point2,func,n)
-%UNTITLED4 此处显示有关此函数的摘要
-%   此处显示详细说明
-
-end
